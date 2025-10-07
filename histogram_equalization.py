6e30de27dcb3791f66aac0b581d767fe7127c550
Inzamul Haque <114326055+Inzamull@users.noreply.github.com>
2025-10-07 21:29:14 +0600

    histogram_equalization.py

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -5,7 +5,6 @@ img = cv2.imread('/home/inzamul/Downloads/rose.jpg', 0)
 
 equalized = cv2.equalizeHist(img)
 
-# 3. Show images
 plt.figure(figsize=(10,5))
 
 plt.subplot(2,2,1)
@@ -16,12 +15,10 @@ plt.subplot(2,2,2)
 plt.title("Equalized Image")
 plt.imshow(equalized, cmap='gray')
 
-#Histogram before
 plt.subplot(2,2,3)
 plt.title("Original Histogram")
 plt.hist(img.ravel(), 256, [0,256])
 
-#Histogram after
 plt.subplot(2,2,4)
 plt.title("Equalized Histogram")
 plt.hist(equalized.ravel(), 256, [0,256])
